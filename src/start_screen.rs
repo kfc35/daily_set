ca8ed3dc749f856796ca0092935324d6b7059022
Kevin Chen <chen.kevin.f@gmail.com>
2026-07-03 01:20:31 -0400

    fix the start screen for the most part

--- src/start_screen.rs
+++ src/start_screen.rs
@@ -6,7 +6,7 @@ use bevy::{
     math::UVec2,
     picking::prelude::*,
     scene::prelude::*,
-    text::{FontSize, TextColor, TextFont},
+    text::{FontSize, TextColor, TextFont, TextLayout, Justify},
     ui::prelude::*,
     ui_widgets::Button,
 };
@@ -20,60 +20,23 @@ use crate::{
 pub struct StartScreen;
 
 pub fn start_screen(commands: &mut Commands, state: &Res<GameState>) {
+    let date = format!("For: {}", &state.date);
     commands.queue_spawn_scene(bsn! {
-        // TODO this doesn't need to be a grid... it's just a column.
         Node {
-            display: Display::Grid,
-            grid_template_rows: vec![RepeatedGridTrack::flex(2, 1.)],
-            justify_content: JustifyContent::Center,
-            align_content: AlignContent::Center,
-            width: percent(100),
-            height: percent(100),
+            display: Display::Flex,
+            flex_direction: FlexDirection::Column,
+            justify_content: JustifyContent::SpaceEvenly,
+            align_content: AlignContent::Default,
+            align_items: AlignItems::Center,
+            width: percent(90),
+            height: percent(90),
+            left: percent(5),
+            top: percent(5),
         }
         StartScreen
         Children [
             logo(),
-            menu(state.date.clone())
-        ]
-    });
-}
 
-fn logo() -> impl Scene {
-    bsn! {
-        Node {
-            display: Display::Flex,
-            height: percent(100),
-            justify_content: JustifyContent::Center,
-            align_content: AlignContent::Center,
-        }
-        Children [
-            Node {
-                // If this uses percent(), it's a little bugged.
-                // Should probably investigate why.
-                width: vw(90),
-            }
-            ImageNode {
-                image: "logo.png"
-            }
-        ]
-    }
-}
-
-fn menu(date: String) -> impl Scene {
-    bsn! {
-        // TODO have to clean this up. Don't use grid.. it's just a column.
-        Node {
-            display: Display::Grid,
-            grid_template_rows: vec![
-                GridTrack::flex(2.),
-                GridTrack::flex(2.),
-                GridTrack::flex(1.),
-            ],
-            height: percent(100),
-            align_content: AlignContent::Center,
-            justify_content: JustifyContent::Center,
-        }
-        Children [
             // Start Button
             button("menu/start_button.png")
             on(|_: On<Pointer<Click>>, mut state: ResMut<GameState>, mut commands: Commands,
@@ -96,57 +59,65 @@ fn menu(date: String) -> impl Scene {
                 align_content: AlignContent::Center
             }
             Children [
-                Text::new(format!("For: {}", date))
+                Text::new(date)
                 TextFont {
                     font_size: FontSize::Px(20.0),
                 }
                 TextColor(GREEN_COLOR)
+                TextLayout::justify(Justify::Center)
             ]
         ]
+    });
+}
+
+fn logo() -> impl Scene {
+    bsn! {
+        Node {
+            height: percent(50),
+            width: percent(80),
+        }
+        ImageNode {
+            image: "logo.png"
+        }
     }
 }
 
 fn button(path: &'static str) -> impl Scene {
     bsn! {
         Button
-            Node {
-                justify_content: JustifyContent::Center,
-                align_content: AlignContent::Center,
-                padding: UiRect::axes(percent(5), percent(4)),
-                margin: UiRect::axes(percent(0), percent(5)),
-                border: UiRect::all(px(5)),
-            }
-            BorderColor::all(GREEN_COLOR)
-            on_handler_style_button_image::<Over>(TEXT_OVER_COLOR, 1)
-            on_handler_style_button_image::<Press>(TEXT_PRESS_COLOR, 2)
-            on_handler_style_button_image::<Release>(TEXT_OVER_COLOR, 1)
-            on_handler_style_button_image::<Out>(GREEN_COLOR, 0)
-            Children[
-                Node {
-                  // If this uses percent(), it's a little bugged.
-                  // Should probably investigate why.
-                  min_width: vw(33),
-                  height: percent(100),
-                  min_height: px(36)
-                }
-                // Unsure how to do this by just having to modify the texture_atlas of the ImageNode
-                template(move |context| {
-                    let layout = TextureAtlasLayout::from_grid(UVec2::new(48, 16), 1, 3, None, None);
-                    let layout_handle = context.resource_mut::<Assets<TextureAtlasLayout>>().add(layout);
-                    let texture_atlas = TextureAtlas {
-                        layout: layout_handle,
-                        index: 0,
-                    };
-                    Ok(ImageNode {
-                        image: context.resource::<AssetServer>().load(path),
-                        texture_atlas: Some(texture_atlas),
-                        ..Default::default()
-                    })
+        Node {
+            border: UiRect::all(px(5)),
+            height: percent(20),
+            width: percent(50),
+        }
+        BorderColor::all(GREEN_COLOR)
+        on_handler_style_button_image::<Over>(TEXT_OVER_COLOR, 1)
+        on_handler_style_button_image::<Press>(TEXT_PRESS_COLOR, 2)
+        on_handler_style_button_image::<Release>(TEXT_OVER_COLOR, 1)
+        on_handler_style_button_image::<Out>(GREEN_COLOR, 0)
+        // Unsure how to do this by just having to modify the texture_atlas of the ImageNode
+        // Children [
+            // Node
+            template(move |context| {
+                let layout = TextureAtlasLayout::from_grid(UVec2::new(48, 16), 1, 3, None, None);
+                let layout_handle = context.resource_mut::<Assets<TextureAtlasLayout>>().add(layout);
+                let texture_atlas = TextureAtlas {
+                    layout: layout_handle,
+                    index: 0,
+                };
+                Ok(ImageNode {
+                    image: context.resource::<AssetServer>().load(path),
+                    texture_atlas: Some(texture_atlas),
+                    ..Default::default()
                 })
-            ]
+            })
+        // ]
     }
 }
 
+/// Helper to attach an observer to an entity for the given Pointer Event `E` that changes:
+/// the `BorderColor` of this entity to the provided color and the `texture_atlas` of the
+/// `ImageNode` on this entity and its direct child to use the provided index.
 fn on_handler_style_button_image<E>(
     border_color: bevy::color::Color,
     texture_atlas_index: usize,
@@ -161,14 +132,16 @@ where
             children_query: Query<&Children>,
             mut image_q: Query<&mut ImageNode>| {
             commands.entity(event.entity).insert(BorderColor::all(border_color));
-            let Some(Ok(mut image_node)) = children_query
+            if let Some(Ok(mut image_node)) = children_query
                 .iter_descendants(event.entity)
                 .find(|e| image_q.contains(*e))
                 .map(|e| image_q.get_mut(e))
-            else {
-                return;
-            };
-            if let Some(atlas) = &mut image_node.texture_atlas {
+                && let Some(atlas) = &mut image_node.texture_atlas {
+              atlas.index = texture_atlas_index;
+            }
+
+            if let Some(Ok(mut image_node)) = image_q.get_mut(event.entity).into()
+                && let Some(atlas) = &mut image_node.texture_atlas {
               atlas.index = texture_atlas_index;
             }
         })
